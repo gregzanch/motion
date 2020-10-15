db78ab269ce220f03db73ef79be6b947b267da53
Greg Zanchelli <zanchelli.greg@gmail.com>
2020-10-15 11:30:55 -0400

    shell script not needed

--- concat.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# cat res/out/*.png | ffmpeg -f "rawvideo" -vcodec "rawvideo" -pix_fmt "argb" -s "720x480" -r "30" -i - -vf "format=yuv420p" -vcodec "libx264" -profile:v "high" -preset:v "medium" -crf "18" -movflags "faststart" -y "out.mp4"
-cat res/out/*.png | ffmpeg -f "image2pipe" -s "720x480" -r "30" -i - -vf "format=yuv420p" -vcodec "libx264" -profile:v "high" -preset:v "medium" -crf "18" -movflags "faststart" -y "out.mp4"
